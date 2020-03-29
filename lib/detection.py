bfa72445e30edcfd57c2a2dd852ab7dd7276e66f
Rory Jacob <roryjacob@gmail.com>
2020-03-28 17:48:33 -0700

    Refactor face location into face metadata + add facial features
    This commit adds a new concept of FaceMetadata that includes both the
    bounding box of the face as well as specific facial features. This means
    an effect can learn the details at a per-image level where their facial
    features are, to spooky effect.

--- lib/detection.py
+++ lib/detection.py
@@ -15,7 +15,7 @@ class FaceMetadata(object):
         return self.__face_location
 
     def get_facial_feature_points(self, facial_feature: str) -> [(int, int)]:
-        if facial_feature not in self.__face_features:
+        if facial_feature not in self.__facial_features:
             raise Exception(f'the feature {facial_feature} was not detected')
 
         return self.__facial_features[facial_feature]
@@ -44,6 +44,8 @@ def find_faces_from_array(img_data: np.array) -> List[FaceMetadata]:
         features = face_recognition.face_landmarks(img_data,
                                                    [(top, right, bottom, left)])
 
-        result.append(FaceMetadata((top, right, bottom, left), features))
+        if len(features) != 0:
+            raise Exception(f'unexpected number of faces found: {len(features)}')
+        result.append(FaceMetadata((top, right, bottom, left), features[0]))
 
     return result
