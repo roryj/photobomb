6d4f1608dfb9c56859e7877e24a9730de8531561
Matthew Fong <jmattfong@gmail.com>
2021-10-12 00:06:55 -0700

    Add photo number text to test image

--- test.py
+++ test.py
@@ -15,20 +15,44 @@ def show_webcam(mirror=False):
         ret_val, img = cam.read()
         if mirror:
             img = cv2.flip(img, 1)
-        draw_text(img, '1')
+        draw_countdown_text(img, '5')
+        draw_photo_num_text(img, 'photo 1/4')
         cv2.imshow('my webcam', img)
         key_press = cv2.waitKey(1)
         if ord('\n') == key_press or key_press == ord('\r'):
             break  # esc to quit
     cv2.destroyAllWindows()
 
-def draw_text(img, text="what what in my butt"):
+def draw_countdown_text(img, text="what what in my butt"):
     height, width, _ = img.shape
     font_scale = 7
-    thickness = 4
+    thickness = 7
 
     (text_width, text_height), _ = cv2.getTextSize(text, fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=font_scale, thickness=thickness)
     text_location = (int(width / 2 - text_width / 2), int(height / 2 + text_height / 2))
+
+    cv2.putText(img,
+        text=text,
+        org=text_location,
+        fontFace=cv2.FONT_HERSHEY_COMPLEX,
+        fontScale=font_scale,
+        color=[0, 0, 0],
+        lineType=cv2.LINE_AA, thickness=thickness)
+    cv2.putText(img,
+        text=text,
+        org=text_location,
+        fontFace=cv2.FONT_HERSHEY_COMPLEX,
+        fontScale=font_scale,
+        color=[255, 255, 255],
+        lineType=cv2.LINE_AA, thickness=(thickness - 3))
+
+def draw_photo_num_text(img, text="you wanna do it my butt?"):
+    height, width, _ = img.shape
+    font_scale = 1
+    thickness = 4
+
+    (text_width, text_height), _ = cv2.getTextSize(text, fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=font_scale, thickness=thickness)
+    text_location = (50, 50)
 
     cv2.putText(img,
         text=text,
